a68a963a1ce09e1dc12c6812ce420416f3e11d26
Guesanjo <guesanjoupv@gmail.com>
2022-09-28 16:15:08 +0200

    Hola

--- PictureViewer/Form1.cs
+++ PictureViewer/Form1.cs
@@ -16,5 +16,49 @@ namespace PictureViewer
         {
             InitializeComponent();
         }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // Enseña Open File dialog. Si OK, carga
+            // imagen elegida.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Elimina imagen.
+            pictureBox1.Image = null;
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            // Pone imagen background.
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            // Cierra.
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Si checkeado cambia el SizeMode , osea su propiedad a Stretch en vez de normal
+            // Osea cambia visualizacion del tamaño,forma de la imagen seleccionada
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
     }
 }
